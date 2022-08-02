d872b972c54f9c93603cf451983e98741c417f99
janshermarri <janshermarri@yahoo.com>
2022-08-02 13:04:59 +0500

    LMS-3: Create Django models

--- lms_main/models.py
+++ lms_main/models.py
@@ -7,8 +7,14 @@ class Teacher(models.Model):
     contact = models.CharField(max_length=255, blank=True)
     qualifications = models.CharField(max_length=1000, blank=True)
 
-class Teacher(models.Model):
+    def __str__(self):
+        return (self.user.first_name + " " + self.user.last_name)
+
+class Student(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     address = models.CharField(max_length=255, blank=True)
     contact = models.CharField(max_length=255, blank=True)
     guardian = models.CharField(max_length=255, blank=True)
+
+    def __str__(self):
+        return (self.user.first_name + " " + self.user.last_name)
