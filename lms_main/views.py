ad5522cace02d0e6a8a107392a00cad480f5dc90
janshermarri <janshermarri@yahoo.com>
2022-08-03 18:48:12 +0500

    Teacher creation

--- lms_main/views.py
+++ lms_main/views.py
@@ -1,12 +1,28 @@
+from statistics import quantiles
 from lms_main.models import Teacher, Student
 from rest_framework import viewsets
 from rest_framework.permissions import IsAuthenticated
 from lms_main.serializers import TeacherSerializer, StudentSerializer
+from django.contrib.auth.models import User
+from django.http import JsonResponse
+
 
 class TeacherViewSet(viewsets.ModelViewSet):
     permission_classes = [IsAuthenticated]
     queryset = Teacher.objects.all()
     serializer_class = TeacherSerializer
+
+    def create(self, request, *args, **kwargs):
+        user = User.objects.create(
+            first_name=request.data['first_name'], last_name=request.data['last_name'], email=request.data['email'], username=request.data['username'])
+        teacher = Teacher.objects.create(
+            user=user, address=request.data['address'], contact=request.data['contact'], qualifications=request.data['qualifications'])
+        new_record = TeacherSerializer(teacher)
+        return JsonResponse({"new_record": new_record.data, "status": "new record success"})
+
+    def perform_create(self, serializer):
+        serializer.save()
+
 
 class StudentViewSet(viewsets.ModelViewSet):
     permission_classes = [IsAuthenticated]
