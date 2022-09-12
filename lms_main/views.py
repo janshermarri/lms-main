a2db3e4a5717bde68cf79df8bc025e984b2540c0
janshermarri <janshermarri@yahoo.com>
2022-09-13 02:34:38 +0500

    Changes for student model

--- lms_main/views.py
+++ lms_main/views.py
@@ -6,6 +6,7 @@ from lms_main.serializers import TeacherSerializer, StudentSerializer, UserSeria
 from django.contrib.auth.models import User
 from django.http import JsonResponse
 
+
 class TeacherViewSet(viewsets.ModelViewSet):
     permission_classes = [IsAuthenticated]
     queryset = Teacher.objects.all()
@@ -18,7 +19,7 @@ class TeacherViewSet(viewsets.ModelViewSet):
             user=user, address=request.data['address'], contact=request.data['contact'], qualifications=request.data['qualifications'])
         new_record = TeacherSerializer(teacher)
         return JsonResponse({"new_record": new_record.data, "status": "new record success"})
-    
+
     def destroy(self, request, *args, **kwargs):
         try:
             user = User.objects.get(id=kwargs.get('pk'))
@@ -35,3 +36,22 @@ class StudentViewSet(viewsets.ModelViewSet):
     permission_classes = [IsAuthenticated]
     queryset = Student.objects.all()
     serializer_class = StudentSerializer
+
+    def create(self, request, *args, **kwargs):
+        user = User.objects.create(
+            first_name=request.data['first_name'], last_name=request.data['last_name'], email=request.data['email'], username=request.data['username'])
+        student = Student.objects.create(
+            user=user, address=request.data['address'], contact=request.data['contact'], qualifications=request.data['guardian'])
+        new_record = StudentSerializer(student)
+        return JsonResponse({"new_record": new_record.data, "status": "new record success"})
+
+    def destroy(self, request, *args, **kwargs):
+        try:
+            user = User.objects.get(id=kwargs.get('pk'))
+            user.delete()
+            return JsonResponse({"status": "delete record success"})
+        except:
+            return JsonResponse({"status": "delete record error"}, status=500)
+
+    def perform_create(self, serializer):
+        serializer.save()
