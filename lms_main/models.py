e9352ca86fa9397d971d4e952ed03eb7f691d3a3
janshermarri <janshermarri@yahoo.com>
2022-09-13 13:14:38 +0500

    Fix

--- lms_main/models.py
+++ lms_main/models.py
@@ -24,7 +24,7 @@ class TeacherStudentSession(models.Model):
     student = models.ForeignKey(Student, on_delete=models.CASCADE)
 
     def __str__(self):
-        return f'Teacher: {self.teacher.first_name} {self.teacher.last_name}, Student: {self.student.first_name} {self.student.last_name}'
+        return f'Teacher: {self.teacher.user.first_name} {self.teacher.user.last_name}, Student: {self.student.user.first_name} {self.student.user.last_name}'
 
 
 class Comment(models.Model):
